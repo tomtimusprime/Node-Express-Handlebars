42be3887e110cf8b972267a62c976a1eb21d9e03
Tom Black <tom.black06007@gmail.com>
2020-05-29 18:09:00 -0700

    added MVC architecture and completed the database schema and seed file.

---
+++ db/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Bacon Burger", false), ("Teriyaki Burger", false), ("Protein Style Burger", false), ("Western Burger", false);
---
+++ server.js
@@ -0,0 +1,2 @@
+const express = require("express");
+const connection = require("./config/connection.js");
